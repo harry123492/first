1b2525180636775e254a4b2cf09f3eedd212b24b
JoongGill Lee <harry123492@naver.com>
2021-08-19 20:00:43 +0900

    weather

--- App.js
+++ App.js
@@ -1,26 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import {Alert} from "react-native";
 import Loading from './Loading';
 import * as Location from 'expo-location';
+import axios from 'axios';
+
+const API_KEY = "5913a0af57264c8feaa107e469c6bd57";
+
 
 export default class extends React.Component {
+  state = {
+    isLoading: true
+  };
+  getWeather = async(latitude, longitude) => {
+    const { data } = await axios.get(`http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}&units=metric`)
+    console.log(data);
+  }
   getLocation = async() => {
     try {
       await Location.requestForegroundPermissionsAsync();
-      const location = await Location.getCurrentPositionAsync()  
-      console.log(location);
+      const {coords:{latitude, longitude}} = await Location.getCurrentPositionAsync()
+      this.getWeather(latitude, longitude)
+      this.setState({ isLoading: false });  
     } catch (error) {
       Alert.alert("Can't find you.", "So sad");
     }
   };
-  
   componentDidMount() {
     this.getLocation();
   }
   render(){
-    return (
-      <Loading></Loading>
-    );
+    const { isLoading } = this.state;
+    return isLoading ? <Loading/> : null;
   }
 }
